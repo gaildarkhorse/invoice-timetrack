bb9e7a2f545d7cefba2cb839754be4468fab0683
gaildarkhorse <dreamoduceius@gmail.com>
2023-12-12 03:14:31 +0900

    create-invoice-timetrack

---
+++ server/config/constants.js
@@ -0,0 +1,8 @@
+export const DB_URL = 'mongodb://127.0.0.1:27017/invc'
+export const PORT = 5000
+export const SECRET = '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'
+export const SMTP_HOST = 'smtp.lyckowbackman.se'
+export const SMTP_PORT = 465
+export const SMTP_USER = '4life@lyckowbackman.se'
+export const SMTP_PASS = 'M<3DKyF6E@H!V=ZjC'
+export const EMAIL_FROM = 'dreamoduceius@gmail.com'
